54b85406b652031c417244cbfc6e42d3620eece6
TheGoldenEye <goldeneye@jn11.xyz>
2021-09-04 13:16:48 +0200

    handle existing indexes

--- migrations/004-indexes.sql
+++ migrations/004-indexes.sql
@@ -1,9 +1,9 @@
 -- Up
-CREATE INDEX idx_transactions_height ON transactions (height);
-CREATE INDEX idx_transactions_authorId_height ON transactions (authorId, height);
-CREATE INDEX idx_transactions_senderId_height ON transactions (senderId, height);
-CREATE INDEX idx_transactions_recipientId_height ON transactions (recipientId, height);
-CREATE INDEX idx_transactions_event_addData_height ON transactions (event, addData, height);
+CREATE INDEX IF NOT EXISTS idx_transactions_height ON transactions (height);
+CREATE INDEX IF NOT EXISTS idx_transactions_authorId_height ON transactions (authorId, height);
+CREATE INDEX IF NOT EXISTS idx_transactions_senderId_height ON transactions (senderId, height);
+CREATE INDEX IF NOT EXISTS idx_transactions_recipientId_height ON transactions (recipientId, height);
+CREATE INDEX IF NOT EXISTS idx_transactions_event_addData_height ON transactions (event, addData, height);
 
 -- Down
 DROP INDEX IF EXISTS idx_transactions_height;
