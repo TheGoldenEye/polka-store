2c4e6498110bf6106584ff1bf8931446f7c06c38
TheGoldenEye <goldeneye@jn11.xyz>
2022-05-05 23:28:48 +0200

    filter events needed for fee calculation

--- src/CPolkaStore.ts
+++ src/CPolkaStore.ts
@@ -841,13 +841,18 @@ export class CPolkaStore {
     if (!tx.specVersion)
       return false;
 
-    const fee_old = this.CalcTotalFee_pre9120(ex, tx);    // sum of feeBalances and feeTreasury
+    // filter events: determine only those that are necessary for the fee calculation
+    const events = this.FilterEvents(ex, tx);
+    if (!events.length)
+      return false;
+
+    const fee_old = this.CalcTotalFee_pre9120(events, tx);    // sum of feeBalances and feeTreasury
     let fee = fee_old;
 
     if (tx.specVersion >= 9120) {
-      const fee_new = this.CalcTotalFee_9120(ex, tx);     // consider balances.Withdraw
+      const fee_new = this.CalcTotalFee_9120(events, tx);     // consider balances.Withdraw
 
-      if (fee_new.totalFee)                               // use only, if successfull
+      if (fee_new.totalFee)                                   // use only, if successfull
         fee = fee_new;
 
       // Error Check
@@ -866,28 +871,16 @@ export class CPolkaStore {
   // looks for balance.Deposit method with fee infomation and sets this in tx
   // calculates totalFee as sum of feeBalances and feeTreasury
   // the feeBalances goes to the block author, feeTreasury to the treasury
-  private CalcTotalFee_pre9120(ex: IExtrinsic, tx: TTransaction): TFee {
+  private CalcTotalFee_pre9120(events: ISanitizedEvent[], tx: TTransaction): TFee {
     const ret: TFee = {
       totalFee: undefined,
       feeBalances: undefined,
       feeTreasury: undefined
     };
 
-    let v = BigInt(0);
-    let v_last = BigInt(0);
-    ex.events.forEach((ev: ISanitizedEvent) => {
+    events.forEach((ev: ISanitizedEvent) => {
       if (ev.method == 'balances.Deposit' && ev.data[0].toString() == tx.authorId) {  // calc fees for block author
-        v = BigInt(ev.data[1].toString());
-
-        // in runtime 912x there are duplicate 'balances.Deposit'-entries due to a bug
-        // we filter out duplicate entries
-        if (tx.specVersion && tx.specVersion > 9120 && tx.specVersion < 9130) {
-          if (v == v_last)    // duplicate value
-            v = BigInt(0);
-          v_last = v;
-        }
-
-        ret.feeBalances = (ret.feeBalances || BigInt(0)) + v;
+        ret.feeBalances = (ret.feeBalances || BigInt(0)) + BigInt(ev.data[1].toString());
       }
       else if (ev.method == 'treasury.Deposit') {
         ret.feeTreasury = (ret.feeTreasury || BigInt(0)) + BigInt(ev.data[0].toString());
@@ -901,14 +894,14 @@ export class CPolkaStore {
 
   // --------------------------------------------------------------
   // starting with runtime 9120 there is a balances.Withdraw event containing the total fee
-  private CalcTotalFee_9120(ex: IExtrinsic, tx: TTransaction): TFee {
+  private CalcTotalFee_9120(events: ISanitizedEvent[], tx: TTransaction): TFee {
     const ret: TFee = {
       totalFee: undefined,
       feeBalances: undefined,
       feeTreasury: undefined
     };
 
-    ex.events.forEach((ev: ISanitizedEvent) => {
+    events.forEach((ev: ISanitizedEvent) => {
       // the fee payment of the sender (initial fee):
       if (ev.method == 'balances.Withdraw' && ev.data[0].toString() == tx.senderId) {
         if (!ret.totalFee)  // first balances.Withdraw only
@@ -928,6 +921,43 @@ export class CPolkaStore {
 
     if (ret.totalFee)
       ret.feeBalances = ret.totalFee - (ret.feeTreasury || BigInt(0));
+    return ret;
+  }
+
+  // --------------------------------------------------------------
+  // filter all events according to necessity for the fee calculation
+  // needed: balances.Withdraw, balances.Deposit, treasury.Deposit
+  // additionally filter uot needed balances.Deposit events:
+  // 1. 'balances.Deposit' events which are related to an "staking.Rewarded" event
+  // 2. duplicate 'balances.Deposit' events (runtime 9120...9130 only)
+  private FilterEvents(ex: IExtrinsic, tx: TTransaction): ISanitizedEvent[] {
+    const ret: ISanitizedEvent[] = [];
+
+    let last: ISanitizedEvent | undefined;
+    ex.events.forEach((ev: ISanitizedEvent) => {
+      if (ev.method == 'balances.Withdraw' || ev.method == 'treasury.Deposit') {
+        ret.push(last = ev);
+        return;
+      }
+
+      // case 1: 'balances.Deposit' events which are related to an "staking.Rewarded" event
+      if (ev.method == 'staking.Rewarded') {
+        if (last && last.method == 'balances.Deposit' && ev.data[1].toString() == last.data[1].toString()) {
+          ret.pop();
+          last = undefined;
+        }
+        return;
+      }
+
+      // case 2: duplicate 'balances.Deposit' events
+      if (ev.method == 'balances.Deposit') {
+        if (tx.specVersion && tx.specVersion > 9120 && tx.specVersion < 9130 &&
+          last && last.method == 'balances.Deposit' && ev.data[1].toString() == last.data[1].toString())
+          return;
+        else
+          ret.push(last = ev);
+      }
+    });
     return ret;
   }
 
